dccbe307c811d73297a3bf8f99f4d069d2b90762
shaivmehta5-jpg <shaivmehta5@gmail.com>
2025-08-11 18:59:27 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">
+      Tailwind is working!
+    </h1>
+  );
+}
